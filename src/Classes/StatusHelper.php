e3a49b33a3c30977a35f1ada5488dbb7d4805596
Mr.VACO <brute1904@gmail.com>
2024-06-09 02:37:11 +0500

    Added a helper class for use outside the current plugin

---
+++ src/Classes/StatusHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MrVaco\Status\Classes;
+
+use MrVaco\Status\Models\Group;
+use MrVaco\Status\Models\Status;
+
+class StatusHelper
+{
+    static function ACTIVE($groupSlug): ?Status
+    {
+        return self::getGroupBySlug($groupSlug)->statuses->firstWhere('active', true);
+    }
+
+    static function DISABLED($groupSlug): ?Status
+    {
+        return self::getGroupBySlug($groupSlug)->statuses->firstWhere('disabled', true);
+    }
+
+    static function DRAFT($groupSlug): ?Status
+    {
+        return self::getGroupBySlug($groupSlug)->statuses->firstWhere('draft', true);
+    }
+
+    static function LIST(string $slug, bool $swap = false): array
+    {
+        $data = Status::query()->whereHas('group', function ($query) use ($slug) {
+            $query->where('slug', $slug);
+        })->get();
+
+        return $swap
+            ? $data->pluck('id', 'name')->toArray()
+            : $data->pluck('name', 'id')->toArray();
+    }
+
+    static function getGroupBySlug(string $slug): ?Group
+    {
+        return Group::query()->where('slug', $slug)->firstOrFail();
+    }
+}
